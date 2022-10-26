4f9ffeb61b7de89b64144b17596da7acc77eb682
shuriuss <104681622+shuriuss@users.noreply.github.com>
2022-10-26 13:59:17 +0300

    add code index.js

--- contacts.js
+++ contacts.js
@@ -1,42 +1,65 @@
+const { table } = require("console");
 const path = require("path");
-const fs = require("fs");
-const { v4: uuidv4 } = require('uuid');
+const fs = require("fs").promises;
+const { v4: uuidv4 } = require("uuid");
 
 const contactsPath = path.join(process.cwd(), "db", "contacts.json");
 
-function listContacts() {
-  const result = fs.readFileSync(contactsPath, "utf-8");
+async function contscts() {
+  const response = await fs.readFile(contactsPath, "utf-8");
+  const result = JSON.parse(response);
   return result;
 }
 
-function getContactById(contactId) {
-  const arr = JSON.parse(listContacts());
-  arr.map((el) => {
-    if (JSON.parse(el.id) === contactId) {
-      console.log(el);
-      return el;
-    }
-  });
+async function listContacts() {
+  try {
+    const response = await contscts();
+    console.table(response);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-function removeContact(contactId) {
-  const arr = JSON.parse(listContacts());
-  const newArr = arr.filter((el) => JSON.parse(el.id) !== contactId);
-  fs.writeFileSync(contactsPath, JSON.stringify(newArr));
+async function getContactById(contactId) {
+  try {
+    const arr = await contscts();
+    arr.forEach((el, i) => {
+      if (el.id === contactId) {
+        console.table(el);
+        return;
+      }
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-function addContact(name, email, phone) {
-  const arr = JSON.parse(listContacts());
-  arr.push({
-    id: uuidv4(),
-    name: name,
-    email: email,
-    phone: phone,
-  });
-  fs.writeFileSync(contactsPath, JSON.stringify(arr));
+async function removeContact(contactId) {
+  try {
+    const arr = await contscts();
+    const newArr = arr.filter((el) => el.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(newArr));
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-addContact("Thomas Lucas", "nec@Nulla.com", "(555) 555-3222");
+// removeContact("5");
+
+async function addContact(name, email, phone) {
+  try {
+    const arr = await contscts();
+    arr.push({
+      id: uuidv4(),
+      name: name,
+      email: email,
+      phone: phone,
+    });
+    fs.writeFile(contactsPath, JSON.stringify(arr));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
 module.exports = {
   listContacts,
